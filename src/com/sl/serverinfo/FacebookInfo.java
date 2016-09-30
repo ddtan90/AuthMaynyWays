639d42f99c30518fcc051ee0463e091c2d16c9c4
ddtan90 <ddtan90@gmail.com>
2016-09-30 07:22:26 +0700

    remove unescessary from server info

--- src/com/sl/serverinfo/FacebookInfo.java
+++ src/com/sl/serverinfo/FacebookInfo.java
@@ -1,15 +1,12 @@
 package com.sl.serverinfo;
 
-public class FacebookInfo extends ServerInfo{
-	
+public class FacebookInfo extends ServerInfo {
+
 	public FacebookInfo() {
 		setAppId("1745111929082181");
 		setSecretKey("bbaae4aca80cd18c7a34952c917e340a");
 		setRedirectUrl("http://localhost:8080/GoogleAuth/facebookcallback");
-		
-		
-		setTokenUrl("https://accounts.google.com/o/oauth2/token");
-		setInfoUrl("https://www.googleapis.com/oauth2/v1/userinfo?access_token=");
+
 	}
 
 }
